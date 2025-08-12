9f23a1769c3f32ef8f2fbe092c6241bdec367073
Elli Cano <melongem64@gmail.com>
2025-08-11 19:56:50 -0500

    finished warning page for LISA shrine

--- scripts/warning.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("container").style.display = "block";
-document.getElementById("close").onclick = function() {
-  document.getElementById("container").style.display = "none";
-};
